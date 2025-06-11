d181a7c3cd9e3c9ea47a67d327d978792872bd27
norihiro400 <norihiro2002@gmail.com>
2025-06-11 16:47:35 +0900

    プロフィール画面#2

--- src/main/java/com/example/todo/controller/profile/ProfileController.java
+++ src/main/java/com/example/todo/controller/profile/ProfileController.java
@@ -4,7 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.todo.controller.tasks.TaskDTO;
+import com.example.todo.service.login.UserEntity;
 import com.example.todo.service.login.UserService;
+import com.example.todo.service.tasks.TaskService;
 
 
 @Controller
@@ -13,10 +17,16 @@ public class ProfileController {
 
     @Autowired
     UserService userService;
+    @Autowired
+    TaskService taskService;
     
     @GetMapping("")
     public String profile(Model model){
         String username = userService.getusername();
+        UserEntity user = userService.findByUsername(username);
+        var taskList = taskService.findByuserId(user.getId()).stream().map(TaskDTO::toDTO).toList();
+        int taskCount = taskList.size();
+        model.addAttribute("taskCount", taskCount);
         model.addAttribute(username, username);
         return "profile/profile";
     }
