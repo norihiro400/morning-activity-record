3b0dd2088ab95934d486e3d76b6c15f22fb9ebf9
norihiro400 <norihiro2002@gmail.com>
2025-09-18 22:22:19 +0900

    followEntityのidフィールドにIdアノテーション付与

--- src/main/java/com/example/todo/service/follow/FollowEntity.java
+++ src/main/java/com/example/todo/service/follow/FollowEntity.java
@@ -1,6 +1,9 @@
 package com.example.todo.service.follow;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -8,6 +11,8 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public class FollowEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private final Long id;
     private final Long followerId; // フォロワーのユーザーID  
     private final Long followedId; // フォローされるユーザーID
