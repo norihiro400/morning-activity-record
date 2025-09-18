abb2f75a4b85a35278ab786e3756953ce8a3f959
norihiro400 <norihiro2002@gmail.com>
2025-09-18 23:42:34 +0900

    フォロワー　フォロー中人数表示

--- src/main/java/com/example/todo/repository/FollowUserRepositoryImpl.java
+++ src/main/java/com/example/todo/repository/FollowUserRepositoryImpl.java
@@ -1,5 +1,8 @@
 package com.example.todo.repository;
 
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +12,12 @@ import com.example.todo.service.follow.FollowEntity;
 public interface FollowUserRepositoryImpl extends JpaRepository<FollowEntity, Long> {
 
     boolean existsByFollowerIdAndFollowedId(Long followingId, Long followedId);
+
+    List<FollowEntity> findAllByFollowerId(Long userId);
+
+    List<FollowEntity> findAllByFollowedId(Long userid);
+    
+    long countByFollowerId(Long userId);   // フォロー中の人数
+    long countByFollowedId(Long userId);   // フォロワーの人数
 
 } 
