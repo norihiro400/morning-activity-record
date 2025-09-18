c78fb7be4a9ca203b7343abb7b8443c831b66b36
norihiro400 <norihiro2002@gmail.com>
2025-09-18 22:34:37 +0900

    followEntityのTableアノテーション追加

--- src/main/java/com/example/todo/service/follow/FollowEntity.java
+++ src/main/java/com/example/todo/service/follow/FollowEntity.java
@@ -5,10 +5,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Entity
+@Table(name = "follow")
 @Getter
 @RequiredArgsConstructor
 public class FollowEntity {
