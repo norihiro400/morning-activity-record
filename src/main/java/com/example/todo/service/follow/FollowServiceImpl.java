2177c95e37ba5e2d999e24a8e3c9316356d45d4d
norihiro400 <norihiro2002@gmail.com>
2025-09-18 22:27:30 +0900

    followServiceのBean定義漏れ修正

--- src/main/java/com/example/todo/service/follow/FollowServiceImpl.java
+++ src/main/java/com/example/todo/service/follow/FollowServiceImpl.java
@@ -1,7 +1,10 @@
 package com.example.todo.service.follow;
 
+import org.springframework.stereotype.Service;
+
 import com.example.todo.repository.FollowUserRepositoryImpl;
 
+@Service
 public class FollowServiceImpl implements FollowService {
     private final FollowUserRepositoryImpl followUserRepositoryImpl;
 
